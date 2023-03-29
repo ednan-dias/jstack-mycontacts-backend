faeaaf88e39a559502168fe6b7e39105df34e903
Ednan Dias <ednan.741@gmail.com>
2023-03-29 15:09:13 -0300

    add ipv4 an const

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 require('express-async-errors');
 const express = require('express');
 
+const { networkInterfaces } = require('os');
 const cors = require('./app/middlewares/cors');
 const errorHandler = require('./app/middlewares/errorHandler');
 
 const routes = require('./routes');
 
+const nets = networkInterfaces();
+
+const ipv4 = nets.enp7s0[0].address;
+
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
 
 app.use(express.json());
 app.use(cors);
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(3001, () => console.log('🔥 Server is running at http://localhost:3001'));
+app.listen(3001, () => console.log(`🔥 Server is running at http://${ipv4}:3001`));
